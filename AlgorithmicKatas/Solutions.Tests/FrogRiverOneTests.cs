f67f05db67ac57f63c504eef4a861f50aa57485a
Michael Clark <michael.clark@red-gate.com>
2016-04-15 13:47:04 +0100

    Solve FrogRiverOne

---
+++ AlgorithmicKatas/Solutions.Tests/FrogRiverOneTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Solutions.Tests
+{
+    public class FrogRiverOneTests
+    {
+        [TestCase(1, new[] { 1 }, 0)]
+        [TestCase(2, new[] { 1 }, -1)]
+        [TestCase(2, new[] { 1, 2 }, 1)]
+        [TestCase(5, new[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 6)]
+        public void Inputs(int X, int[] A, int K)
+        {
+            var frogRiverOne = new FrogRiverOne.Solution();
+            var solution = frogRiverOne.solution(X, A);
+            Assert.That(solution, Is.EqualTo(K));
+        }
+    }
+}
---
+++ AlgorithmicKatas/Solutions/FrogRiverOne/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.FrogRiverOne
+{
+    public class Solution
+    {
+        public int solution(int X, int[] A)
+        {
+            var remainingPositions = new HashSet<int>(Enumerable.Range(1, X));
+
+            for (int timeIndex = 0; timeIndex < A.Length; timeIndex++)
+            {
+                var positionAtTime = A[timeIndex];
+                if (remainingPositions.Contains(positionAtTime))
+                {
+                    remainingPositions.Remove(positionAtTime);
+                    if (remainingPositions.Count == 0)
+                    {
+                        return timeIndex;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
