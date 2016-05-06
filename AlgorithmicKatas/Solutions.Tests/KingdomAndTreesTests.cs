9dc542dd2f635e410d803ac4e9f375161cb03281
Michael Clark <michael.clark@red-gate.com>
2016-05-06 17:24:21 +0100

    Solve KingdomAndTrees

---
+++ AlgorithmicKatas/Solutions.Tests/KingdomAndTreesTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Solutions.KingdomAndTrees;
+
+namespace Solutions.Tests
+{
+    class KingdomAndTreesTests
+    {
+        [TestCase(new[] { 1 }, 0)]
+        [TestCase(new[] { 1, 1 }, 1)]
+        [TestCase(new[] { 1, 2 }, 0)]
+        [TestCase(new[] { 2, 1 }, 1)]
+        [TestCase(new[] { 3, 2, 1 }, 2)]
+
+        [TestCase(new[] { 9, 5, 11 }, 3)]
+        [TestCase(new[] { 5, 8 }, 0)]
+        [TestCase(new[] { 1, 1, 1, 1, 1 }, 4)]
+        [TestCase(new[] { 548, 47, 58, 250, 2012 }, 251)]
+
+        [TestCase(new[] { 1000000000, 1 }, 500000000)]
+        [TestCase(new[] { 1000000000, 3 }, 499999999)]
+        public void CorrectHeightReturned(int[] heights, int expectedMinHeight)
+        {
+            var minHeight = Problem.GetMinLevel(heights);
+            Assert.That(minHeight, Is.EqualTo(expectedMinHeight));
+        }
+    }
+}
---
+++ AlgorithmicKatas/Solutions/KingdomAndTrees/Problem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Solutions.KingdomAndTrees
+{
+    public static class Problem
+    {
+        private const int MaxHeight = 1000000000;
+
+        private const int MinX = 0;
+        private const int MaxX = MaxHeight;
+
+        public static int GetMinLevel(int[] heights)
+        {
+            if (heights.Length < 2)
+            {
+                return 0;
+            }
+
+            return BinarySearch(MinX, (MinX + MaxX) / 2, MaxX, heights);
+        }
+
+        public static int BinarySearch(int minX, int X, int maxX, int[] heights)
+        {
+            if (minX == maxX)
+            {
+                return minX;
+            }
+
+            if (HasSolution(X, heights))
+            {
+                return BinarySearch(minX, (minX + X) / 2, X, heights);
+            }
+
+            return BinarySearch(X + 1, (X + 1 + maxX) / 2, maxX, heights);
+        }
+
+        private static bool HasSolution(int X, int[] heights)
+        {
+            int lastHeight = Math.Max(heights[0] - X, 1);
+
+            foreach (var height in heights.Skip(1))
+            {
+                if (height + X > lastHeight)
+                {
+                    lastHeight = Math.Max(height - X, lastHeight + 1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
