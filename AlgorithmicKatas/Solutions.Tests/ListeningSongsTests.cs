4a394cb25dd44b103f1e255e43c6c857adf7670a
Michael Clark <michael.clark@red-gate.com>
2016-05-06 16:00:02 +0100

    Solve ListeningSongs problem with procedural code

---
+++ AlgorithmicKatas/Solutions.Tests/ListeningSongsTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Solutions.ListeningSongs;
+
+namespace Solutions.Tests
+{
+    class ListeningSongsTests
+    {
+        [TestCase(new int[0], new int[0], 0, 0, 0)]
+        [TestCase(new[] { 1 }, new int[0], 0, 0, 0)]
+        [TestCase(new[] { 1 }, new int[0], 1, 0, 1)]
+        [TestCase(new[] { 1 }, new[] { 1 }, 2, 0, 2)]
+        [TestCase(new[] { 300, 200, 100 }, new[] { 400, 500, 600 }, 17, 1, 4)]
+        [TestCase(new[] { 300, 200, 100 }, new[] { 400, 500, 600 }, 10, 1, 2)]
+        [TestCase(new[] { 60, 60, 60 }, new[] { 60, 60, 60 }, 5, 2, 5)]
+        [TestCase(new[] { 120, 120, 120, 120, 120 }, new[] { 60, 60, 60, 60, 60, 60 }, 10, 3, 7)]
+        [TestCase(new[] { 196, 147, 201, 106, 239, 332, 165, 130, 205, 221, 248, 108, 60 }, new[] { 280, 164, 206, 95, 81, 383, 96, 255, 260, 244, 60, 313, 101 }, 60, 3, 22)]
+        [TestCase(new[] { 100, 200, 300 }, new[] { 100, 200, 300 }, 2, 1, -1)]
+        [TestCase(new[] { 100, 200, 300, 400, 500, 600 }, new[] { 100, 200 }, 1000, 3, -1)]
+        public void test(int[] durations1, int[] durations2, int minutes, int T, int expectedMaxSongs)
+        {
+            var maxSongs = Problem.GetMaxSongs(durations1, durations2, minutes, T);
+            Assert.That(maxSongs, Is.EqualTo(expectedMaxSongs));
+
+            var maxSongs2 = Problem.GetMaxSongs(durations2, durations1, minutes, T);
+            Assert.That(maxSongs2, Is.EqualTo(expectedMaxSongs));
+        }
+    }
+}
---
+++ AlgorithmicKatas/Solutions/ListeningSongs/Problem.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Solutions.ListeningSongs
+{
+    /// <remarks>
+    /// Test cases generated with 
+    /// ({[^}]+})\r\n({[^}]+})\r\n(\d+)\r\n(\d+)\r\nReturns: (-?\d+)
+    /// to
+    /// [TestCase(new []$1, new []$2, $3, $4, $5)]
+    /// </remarks>
+    public static class Problem
+    {
+        public static int GetMaxSongs(int[] durations1, int[] durations2, int minutes, int T)
+        {
+            var seconds = minutes * 60;
+
+            if (durations1.Length < T || durations2.Length < T)
+            {
+                return -1;
+            }
+
+            Array.Sort(durations1);
+            int albumIndex1 = 0;
+
+            Array.Sort(durations2);
+            int albumIndex2 = 0;
+
+            int totalSongs = 0;
+
+            for (int mandatoryIndex = 0; mandatoryIndex < T; ++mandatoryIndex)
+            {
+                var songDuration1 = durations1[albumIndex1];
+                if (songDuration1 > seconds)
+                {
+                    return -1;
+                }
+
+                totalSongs += 1;
+                seconds -= songDuration1;
+                albumIndex1 += 1;
+
+                var songDuration2 = durations2[albumIndex2];
+                if (songDuration2 > seconds)
+                {
+                    return -1;
+                }
+
+                totalSongs += 1;
+                seconds -= songDuration2;
+                albumIndex2 += 1;
+            }
+
+            while (albumIndex1 < durations1.Length || albumIndex2 < durations2.Length)
+            {
+                if (albumIndex1 < durations1.Length && albumIndex2 < durations2.Length)
+                {
+                    var songDuration1 = durations1[albumIndex1];
+                    var songDuration2 = durations2[albumIndex2];
+
+                    if (songDuration1 < songDuration2)
+                    {
+                        if (songDuration1 > seconds)
+                        {
+                            return totalSongs;
+                        }
+
+                        totalSongs += 1;
+                        seconds -= songDuration1;
+                        albumIndex1 += 1;
+                    }
+                    else
+                    {
+                        if (songDuration2 > seconds)
+                        {
+                            return totalSongs;
+                        }
+
+                        totalSongs += 1;
+                        seconds -= songDuration2;
+                        albumIndex2 += 1;
+                    }
+                }
+                else if (albumIndex1 < durations1.Length)
+                {
+                    var songDuration1 = durations1[albumIndex1];
+                    if (songDuration1 > seconds)
+                    {
+                        return totalSongs;
+                    }
+
+                    totalSongs += 1;
+                    seconds -= songDuration1;
+                    albumIndex1 += 1;
+                }
+                else
+                {
+                    var songDuration2 = durations2[albumIndex2];
+                    if (songDuration2 > seconds)
+                    {
+                        return totalSongs;
+                    }
+
+                    totalSongs += 1;
+                    seconds -= songDuration2;
+                    albumIndex2 += 1;
+                }
+            }
+
+            return totalSongs;
+        }
+    }
+}
