149851abcdaccbfdcec65481f50f71fe5bdd7f7a
Michael Clark <michael.clark@red-gate.com>
2016-06-17 16:37:43 +0100

    Add naive solution to max counters

---
+++ AlgorithmicKatas/Solutions.Tests/MaxCountersTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Solutions.Tests
+{
+    class MaxCountersTests
+    {
+        [TestCase(0, new int[0], new int[0])]
+        [TestCase(1, new int[0], new[] { 0 })]
+        [TestCase(1, new[] { 1 }, new[] { 1 })]
+        [TestCase(5, new[] { 3, 4, 4, 6, 1, 4, 4 }, new[] { 3, 2, 2, 4, 2 })]
+        public void SolutionCorrect(int N, int[] A, int[] expectedCounters)
+        {
+            var actualCounters = new MaxCounters.Solution().solution(N, A);
+            Assert.That(actualCounters, Is.EqualTo(expectedCounters));
+        }
+    }
+}
---
+++ AlgorithmicKatas/Solutions/MaxCounters/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace Solutions.MaxCounters
+{
+    public class Solution
+    {
+        public int[] solution(int N, int[] A)
+        {
+            if (N < 1)
+            {
+                return new int[0];
+            }
+
+            var counters = new int[N];
+
+            foreach (var a in A)
+            {
+                if (a == N + 1)
+                {
+                    var element = counters.Max();
+                    counters = Enumerable.Repeat(element, N).ToArray();
+                }
+                else
+                {
+                    counters[a - 1]++;
+                }
+            }
+
+            return counters;
+        }
+    }
+}
