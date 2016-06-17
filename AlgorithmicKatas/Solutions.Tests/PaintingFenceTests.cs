44427c3e6eebc3b2dd0afd305961bd4238bd4544
Michael Clark <michael.clark@red-gate.com>
2016-06-17 17:07:34 +0100

    Add PaintingFence problem

---
+++ AlgorithmicKatas/Solutions.Tests/PaintingFenceTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Solutions.Tests
+{
+    class PaintingFenceTests
+    {
+        [TestCase(1, new[] { 1 }, 1)]
+        [TestCase(2, new[] { 1, 1 }, 1)]
+        [TestCase(3, new[] { 1, 2, 1 }, 2)]
+        public void SolutionCorrect(int N, int[] A, int expectedStrokes)
+        {
+            var actualStrokes = new PaintingFence.Solution().solution(N, A);
+            Assert.That(actualStrokes, Is.EqualTo(expectedStrokes));
+        }
+    }
+}
---
+++ AlgorithmicKatas/Solutions/PaintingFence/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace Solutions.PaintingFence
+{
+    public class Solution
+    {
+        public int solution(int N, int[] A)
+        {
+
+            return 1;
+        }
+    }
+}
