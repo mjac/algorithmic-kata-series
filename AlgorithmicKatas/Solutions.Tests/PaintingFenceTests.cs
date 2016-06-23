4c57cffe2de02e3662e33dc2cb4e02a7eeb6413f
Michael Clark <mjac@mjac.co.uk>
2016-06-23 18:28:41 +0100

    Solve painting fence

--- AlgorithmicKatas/Solutions.Tests/PaintingFenceTests.cs
+++ AlgorithmicKatas/Solutions.Tests/PaintingFenceTests.cs
@@ -4,11 +4,15 @@ namespace Solutions.Tests
 {
     class PaintingFenceTests
     {
-        [TestCase(1, new[] { 1 }, 1)]
-        [TestCase(2, new[] { 1, 1 }, 1)]
-        [TestCase(3, new[] { 1, 2, 1 }, 2)]
-        public void SolutionCorrect(int N, int[] A, int expectedStrokes)
+        [TestCase(new[] { 1 }, 1, TestName = "Single post of height one")]
+        [TestCase(new[] { 2 }, 1, TestName = "Single post of height two")]
+        [TestCase(new[] { 1, 1 }, 1, TestName = "Two posts of height one")]
+        [TestCase(new[] { 1, 2, 1 }, 2, TestName = "Three posts with middle two and edges one")]
+        [TestCase(new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 1, TestName = "One long horizontal stroke")]
+        [TestCase(new[] { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 }, 2, TestName = "Two long horizontal strokes")]
+        public void SolutionCorrect(int[] A, int expectedStrokes)
         {
+            var N = A.Length;
             var actualStrokes = new PaintingFence.Solution().solution(N, A);
             Assert.That(actualStrokes, Is.EqualTo(expectedStrokes));
         }
--- AlgorithmicKatas/Solutions/PaintingFence/Solution.cs
+++ AlgorithmicKatas/Solutions/PaintingFence/Solution.cs
@@ -1,11 +1,60 @@
-﻿namespace Solutions.PaintingFence
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.PaintingFence
 {
     public class Solution
     {
         public int solution(int N, int[] A)
         {
+            return countStrokes(A);
+        }
 
-            return 1;
+        private int countStrokes(ICollection<int> a)
+        {
+            var width = a.Count;
+            if (width == 1)
+            {
+                return 1;
+            }
+
+            var height = a.Max();
+            if (width <= height)
+            {
+                // We can paint every fence vertically faster in this case
+                return width;
+            }
+
+            var horizontalStrokes = a.Min();
+
+            var remainingPosts = GetRemainingPosts(a.Select(o => o - horizontalStrokes));
+            return horizontalStrokes + remainingPosts.Sum(o => countStrokes(o));
+        }
+
+        private static List<List<int>> GetRemainingPosts(IEnumerable<int> posts)
+        {
+            var postSections = new List<List<int>>();
+
+            var newSectionRequired = true;
+            foreach (var post in posts)
+            {
+                if (post == 0)
+                {
+                    newSectionRequired = true;
+                }
+                else
+                {
+                    if (newSectionRequired)
+                    {
+                        postSections.Add(new List<int>());
+                        newSectionRequired = false;
+                    }
+                    postSections.Last().Add(post);
+                }
+            }
+
+            return postSections;
         }
     }
 }
