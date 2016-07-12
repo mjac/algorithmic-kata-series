2f38aef87e85a545f1b997f7eb76852198b1bf4a
Michael Clark <michael.clark@red-gate.com>
2016-07-12 15:08:31 +0100

    Initial naive solution passing basic tests

--- AlgorithmicKatas/Solutions.Tests/FlowerGardenTests.cs
+++ AlgorithmicKatas/Solutions.Tests/FlowerGardenTests.cs
@@ -5,7 +5,18 @@ namespace Solutions.Tests
 {
     class FlowerGardenTests
     {
-        [TestCase(new[] { 1 }, new[] { 1 }, new[] { 365 }, new[] { 1 })]
+        [TestCase(new[] { 1 }, new[] { 1 }, new[] { 365 }, new[] { 1 }, TestName = "Single flower")]
+        [TestCase(new[] { 2, 1 }, new[] { 1, 1 }, new[] { 365, 365 }, new[] { 1, 2 }, TestName = "Two flowers same period")]
+        [TestCase(new[] { 1, 2 }, new[] { 1, 1 }, new[] { 365, 365 }, new[] { 1, 2 }, TestName = "Two flowers same period reverse input")]
+        [TestCase(new[] { 1, 2 }, new[] { 1, 3 }, new[] { 2, 4 }, new[] { 2, 1 }, TestName = "Two flowers different period")]
+
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 1, 1 }, new[] { 365, 365, 365 }, new[] { 1, 2, 3 }, TestName = "Three flowers same period")]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 3, 5 }, new[] { 2, 4, 6 }, new[] { 3, 2, 1 }, TestName = "Three flowers different period")]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 2, 4 }, new[] { 2, 3, 5 }, new[] { 3, 1, 2 }, TestName = "Three flowers one overlay period")]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 3, 4 }, new[] { 2, 4, 5 }, new[] { 2, 3, 1 }, TestName = "Three flowers another overlay period")]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 2, 3 }, new[] { 2, 3, 4 }, new[] { 1, 2, 3 }, TestName = "Three flowers another overlay period")]
+
+        [TestCase(new[] { 689, 929, 976, 79, 630, 835, 721, 386, 492, 767, 787, 387, 193, 547, 906, 642, 3, 920, 306, 735, 889, 663, 295, 892, 575, 349, 683, 699, 584, 149, 410, 710, 459, 277, 965, 112, 146, 352, 408, 432 }, new[] { 4, 123, 356, 50, 57, 307, 148, 213, 269, 164, 324, 211, 249, 355, 110, 280, 211, 106, 277, 303, 63, 326, 285, 285, 269, 144, 331, 15, 296, 319, 89, 243, 254, 159, 185, 158, 81, 270, 219, 26 }, new[] { 312, 158, 360, 314, 323, 343, 267, 220, 347, 197, 327, 334, 250, 360, 350, 323, 291, 323, 315, 320, 355, 334, 286, 293, 362, 181, 360, 328, 322, 344, 173, 248, 284, 301, 215, 230, 226, 331, 355, 81 }, new[] { 149, 3, 79, 193, 112, 146, 432, 277, 386, 349, 410, 295, 306, 352, 387, 408, 547, 459, 492, 575, 663, 683, 976, 584, 630, 642, 689, 699, 787, 735, 835, 710, 721, 767, 889, 892, 906, 920, 965, 929 }, TestName="Crazy acceptance test")]
         public void FlowerGardenTest(int[] height, int[] bloom, int[] wilt, int[] expectedOrdering)
         {
             var actualOrdering = Solution.getOrdering(height, bloom, wilt);
--- AlgorithmicKatas/Solutions/FlowerGarden/Solution.cs
+++ AlgorithmicKatas/Solutions/FlowerGarden/Solution.cs
@@ -1,10 +1,85 @@
-﻿namespace Solutions.FlowerGarden
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.FlowerGarden
 {
     public class Solution
     {
         public static int[] getOrdering(int[] height, int[] bloom, int[] wilt)
         {
-            return height;
+            var flowers = height.Select((o, i) => new Flower
+            {
+                Height = o,
+                Bloom = bloom[i],
+                Wilt = wilt[i],
+            }).ToArray();
+
+            foreach (var f1 in flowers)
+            {
+                foreach (var f2 in flowers)
+                {
+                    if (f1 != f2 && f1.IsBlockedBy(f2))
+                    {
+                        f1.BlockedBy.Add(f2);
+                        f2.Blocks.Add(f1);
+                    }
+                }
+            }
+
+            var orderedFlowers = new List<Flower>();
+
+            var flowersToPlace = flowers.ToList();
+            while (flowersToPlace.Any())
+            {
+                var min = flowersToPlace.OrderBy(o => o.Height).First(o => !o.IsBlocked);
+                foreach (var blockedFlower in min.Blocks)
+                {
+                    blockedFlower.BlockedBy.Remove(min);
+                }
+
+                flowersToPlace.Remove(min);
+                orderedFlowers.Add(min);
+            }
+
+            return orderedFlowers.Select(o=>o.Height).Reverse().ToArray();
+        }
+
+        private class Flower
+        {
+            public Flower()
+            {
+                Blocks = new List<Flower>();
+                BlockedBy = new List<Flower>();
+            }
+
+            public int Height { get; set; }
+            public int Bloom { get; set; }
+            public int Wilt { get; set; }
+
+            public List<Flower> Blocks { get; set; }
+            public List<Flower> BlockedBy { get; set; }
+
+        public bool IsBlocked => BlockedBy.Any();
+
+            public bool IsBlockedBy(Flower f2)
+            {
+                if (Height > f2.Height)
+                {
+                    return false;
+                }
+
+                if (Bloom > f2.Wilt)
+                {
+                    return false;
+                }
+
+                if (Wilt < f2.Bloom)
+                {
+                    return false;
+                }
+
+                return true;
+            }
         }
     }
 }
